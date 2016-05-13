e6d23f629f753f853d23d25019ee3d0338e4ff32
Tim <uniquelysimilar@yahoo.com>
2016-05-13 15:48:19 -0600

    logout impl

---
+++ app/components/navbar/navbarController.js
@@ -0,0 +1,7 @@
+restApiClientApp.controller('NavbarController', ['$scope', '$window', 'loginService', function($scope, $window, loginService) {
+  $scope.logout = function() {
+    loginService.setCredentials("");
+    $window.location.href = "/";
+  }
+  
+}]);
